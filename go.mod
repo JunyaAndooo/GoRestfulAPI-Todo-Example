49c54156971ad71639aaf0b10e5d1f6b322ac920
安藤順也 <67815204+JunyaAndooo@users.noreply.github.com>
2025-01-22 20:39:35 +0900

    ServerとTestを追加

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JunyaAndooo/GoRestfulAPI-Todo-Example
 
 go 1.23.5
+
+require golang.org/x/sync v0.10.0
--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(
+	err := run(context.Background())
+	if err != nil {
+		log.Printf("failed to terminate server: %+v", err)
+	}
+}
+
+func run(ctx context.Context) (err error) {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 			},
 		),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(
+		func() error {
+			err := s.ListenAndServe()
+			if err != nil && err != http.ErrServerClosed {
+				log.Printf("failed to close: %+v", err)
+				return err
+			}
+
+			return nil
+		},
+	)
+
+	<-ctx.Done()
+	err = s.Shutdown(context.Background())
+	if err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
