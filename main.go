d66c8e30942e2bf5bc634854d5f32c622f90c5fb
安藤順也 <67815204+JunyaAndooo@users.noreply.github.com>
2025-01-26 18:11:44 +0900

    Serverとルーティングを分離

--- main.go
+++ main.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/JunyaAndooo/GoRestfulAPI-Todo-Example/config"
-	"golang.org/x/sync/errgroup"
+	"github.com/JunyaAndooo/GoRestfulAPI-Todo-Example/server"
 )
 
 func main() {
@@ -22,47 +19,22 @@ func main() {
 	}
 }
 
-func run(ctx context.Context) (err error) {
-	// 処理実行中に終了シグナルを受け付けた場合、処理が完全に終わるまでシグナルを待機させる
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
+func run(ctx context.Context) error {
 	cfg, err := config.New()
 	if err != nil {
 		return err
 	}
+
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(
-			func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-			},
-		),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(
-		func() error {
-			err := s.Serve(l)
-			if err != nil && err != http.ErrServerClosed {
-				log.Printf("failed to close: %+v", err)
-				return err
-			}
+	mux := server.NewMux()
+	s := server.NewServer(l, mux)
 
-			return nil
-		},
-	)
-
-	<-ctx.Done()
-	err = s.Shutdown(context.Background())
-	if err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	return eg.Wait()
+	return s.Run(ctx)
 }
